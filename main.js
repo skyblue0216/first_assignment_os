6438880d9c3b516a8f0d200514a000946ad1991f
jace <skbyel@gmail.com>
2024-11-05 20:50:37 +0900

    fourth commit

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
+#!/usr/bin/env node
 
 const lib = require("./lib");
+
+if(process.argv.length<=3){
+    console.log("Insufficient parameter!")
+    process.exit(1);
+}
 
 let command = process.argv[2];
 
 let numbers =process.argv
     .slice(3, process.argv.length)
     .map((n)=>parseFloat(n));
+
+if(numbers.some((n)=>isNaN(n))){
+    console.log("Some arguments are not numbers!");
+    process.exit(1);
+}
 
 let result;
 switch (command) {
